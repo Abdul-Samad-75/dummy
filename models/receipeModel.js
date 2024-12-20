db02c2baec9196df7c236a034baa95c1e6c45b75
Abdul-Samad-75 <abdulsamad7523@gmail.com>
2024-12-20 20:30:59 +0530

    pushed to dummy

---
+++ models/receipeModel.js
@@ -0,0 +1,16 @@
+import { Schema, model } from "mongoose";
+
+const receipeSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    img: { type: String, required: true },
+    mealType: { type: String, required: true },
+    ingredients: { type: [String], required: true },
+    calories: { type: Number, required: true },
+    dietaryPreference: { type: [String], required: true },
+  },
+  { timestamps: true }
+);
+
+const Recipe = model("recipe", receipeSchema);
+export default Recipe;
